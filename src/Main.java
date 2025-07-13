df8bdbec57d814228d233e25ded7b2c7897a7284
zaidrawashdeh911 <zaidrawashdeh2020@gmail.com>
2025-07-13 17:00:29 +0300

    trying stack methods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Stack<Integer> stack = new Stack<>();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        System.out.println(stack);
+        var top = stack.pop();
+        System.out.println(top);
+        System.out.println(stack);
+        var sneak = stack.peek();
+        System.out.println(sneak);
+        System.out.println(stack);
+
     }
 }
